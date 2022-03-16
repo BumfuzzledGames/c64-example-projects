46416e4ca1f24817daa3cacfeea46910bb9f5dd0
Bumfuzzled Games <bumfuzzled.games@gmail.com>
2022-03-15 22:11:42 -0400

    Hello, world

---
+++ 001-hello-world/001-hello-world.asm
@@ -0,0 +1,19 @@
+// display hello world on the screen
+BasicUpstart2(Start)
+
+.encoding "screencode_upper"
+str_hello:
+  .text "HELLO, WORLD!"
+  .byte 0
+
+Start:
+  ldx #0
+!loop:
+  lda str_hello,x
+  beq !end+
+  sta 1024,x
+  inx
+  jmp !loop-
+!end:
+  jmp *
+  
---
+++ config.mak
@@ -0,0 +1,3 @@
+ASM        ?= kickass
+ASM_FLAGS  ?= -vicesymbols -debugdump
+EMU        ?= x64sc
